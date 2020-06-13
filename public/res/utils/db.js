6c2d7f72eeb40c71ce043fc2687c8c4ace5ff666
Andrew Randle-Warren <arandlemiller97@yahoo.com>
2020-06-12 23:44:49 -0500

    I've moved over some code from messing around with learning this stuff. Some of it still needs work but it has the basics of what I'm trying to go for.

--- public/res/utils/db.js
+++ public/res/utils/db.js
@@ -1,3 +1,5 @@
 module.exports = {
+    // TODO: The connection string is on GitHub under the secrets. No one should have access besides
+    //  anyone that is a collaborator on the project
     url: ""
 }
