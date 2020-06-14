d0e46435e5700bd0cefdd52ab151c8f6cb59e122
Andrew Randle-Warren <arandlemiller97@yahoo.com>
2020-06-14 07:56:16 -0500

    I generated the file that will handle the routing for the website. This will be the file that will handle all routing and direct to the webpages we create

---
+++ public/javascripts/appRoutes.js
@@ -0,0 +1,21 @@
+angular.module('appRoutes', [])
+    .config(['$routeProvider', '$locationProvider', ($routeProvider, $locationProvider) => {
+        $routeProvider
+            // home page
+            .when('/', {
+                templateUrl: 'index.html',
+                controller: 'MainController'
+            })
+
+            // students page that will use the StudentController
+            .when('/about', {
+                templateUrl: 'views/about.html',
+                controller: 'AboutController'
+            })
+
+            .when('/review_request', {
+                templateUrl: 'views/review_request',
+                controller: 'RequestController'
+            });
+        $locationProvider.html5Mode(true);
+    }]);
