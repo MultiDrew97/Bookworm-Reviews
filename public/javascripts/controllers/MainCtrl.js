dcf59b7a1a72d168f652cfb1610647863d4d8772
Andrew Randle-Warren <arandlemiller97@yahoo.com>
2020-08-14 10:04:30 -0500

    Changed that I'm going to not store the book cover information in the JSON for the blog post. Some reason using the interpolated string to stream line the cover finding, it deems the ID as undefined instead of what it was supposed to be. I updated the html to utilize the blogID to find the cover since the covers will be in a predetermined location

--- public/javascripts/controllers/MainCtrl.js
+++ public/javascripts/controllers/MainCtrl.js
@@ -1,21 +1,19 @@
 angular.module('MainCtrl', []).controller('MainController', function($scope) {
     $scope.blogs = [{
+        blogID: 123,
         bookTitle: "Vixen",
         bookAuthor: "Jillian Larkin",
         bookPubDate: Date.now(),
-        description: "Sample description of the book",
-        bookCover: "../images/123.png",
-        blogID: "123"
+        description: "Sample description of the book"
     }, {
+        blogID: 234,
         bookTitle: "Ready Player One",
         bookAuthor: "Ernest Cline",
         bookPubDate: Date.now(),
-        description: "Sample description of the book",
-        bookCover: "../images/234.png",
-        blogID: "234"
+        description: "Sample description of the book"
     }];
 
-    // TODO: style the blog posts to be in a 3+ column format and beutify the blog postings
+    // TODO: style the blog posts to be in a 3+ column format and beautify the blog postings
 
     // TODO: figure out how to pass on the blogID from this page to the blog display page and how to get
     //      text from a file based on information from the post db entry
